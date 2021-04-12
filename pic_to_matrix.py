958b51b2d2742628d8ac60c8a2b06e33623e3a08
pandyah5 <pandyahetav1@gmail.com>
2021-04-12 00:03:24 -0400

    Changed background and added clear text

--- pic_to_matrix.py
+++ pic_to_matrix.py
@@ -39,4 +39,4 @@ def generate_hex_code(source, destination):
     f.close()
 
 # Source, destination
-generate_hex_code("D:\\Hetav Documents\\UofT\Academic Planning\\ECE 243\\Final Project\Pictures\\boy_jump_2.jpg", "b1_jump_2.txt")
+generate_hex_code("D:\\Hetav Documents\\UofT\Academic Planning\\ECE 243\\Final Project\Pictures\\cloud_final.png", "back.txt")
